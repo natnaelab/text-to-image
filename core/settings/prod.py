e5222817dff5d98e8347ff207aec389ffefdd993
ntnl <natnael08@protonmail.com>
2023-05-24 08:50:13 +0300

    ...

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,25 +1,14 @@
 from .base import *
+import dj_database_url
 
 DEBUG = False
 
 ALLOWED_HOSTS = ["text-to-image-production.up.railway.app"]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("PGDATABASE"),
-        'USER': os.getenv("PGUSER"),
-        'PASSWORD': os.getenv("PGPASSWORD"),
-        'HOST': os.getenv("PGHOST"),
-        'PORT': os.getenv("PGPORT"),
-    }
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
 }
 
 INSTALLED_APPS.append("whitenoise.runserver_nostatic")
