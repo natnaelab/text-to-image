8f71e062b133449daffa9ea4902030b1bda7a01b
ntnl <natnael08@protonmail.com>
2023-05-27 15:20:47 +0300

    ...

--- bot/handlers/img.py
+++ bot/handlers/img.py
@@ -1,17 +1,27 @@
 from telegram import Update
 from telegram.ext import CallbackContext, MessageHandler, Filters, DispatcherHandlerStop
+from proxy_scraper import proxyScraper, proxyChecker
 
 
 def img(update: Update, context: CallbackContext):
     txt = update.message.text.split(" ")
 
     if len(txt) <= 1:
-        update.message.reply_text("Invalid prompt")
+        update.message.reply_text("No prompt found")
         raise DispatcherHandlerStop
     
     prompt = txt[1]
+    
+    proxyScraper.scrape()
+    proxyChecker.check()
 
-    update.message.reply_text('generating ' + prompt)
+    proxies = open("proxies.txt", 'r')
+    
+    if len(proxies.readlines()) == 0:        
+        update.message.reply_text('unfortunately no proxies found...')
+    else:        
+        update.message.reply_text('found ' + len(proxies.readlines()) + ' proxies...')
+        update.message.reply_text('generating ' + prompt)
 
 
 img_handler = MessageHandler(Filters.command & Filters.regex('^/img'), img)
