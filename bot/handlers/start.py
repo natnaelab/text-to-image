fad9b62ba8ce5d626a88fd3e025df2f422031e3f
ntnl <natnael08@protonmail.com>
2023-05-27 12:47:05 +0300

    ...

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -3,7 +3,24 @@ from telegram.ext import CallbackContext, MessageHandler, Filters
 
 
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text('hello')
+    text = """
+👋 Welcome to the Text-to-Image Generator Bot! 👩‍💻🖼️
+
+I'm here to help you transform your text into beautiful images! Simply type in your desired text, and I'll generate a unique image based on your input.
+
+To get started, follow these steps:
+
+1️⃣ Type your desired text.
+2️⃣ Wait a moment while I create a custom image just for you.
+3️⃣ Once the image is ready, I'll send it right back to you!
+
+If you have any questions or need assistance, feel free to ask! Now go ahead and let your imagination run wild with words! 🚀
+
+Type /help if you need more information about how to use this bot.
+
+Enjoy exploring the power of words and images together! 🎉✨
+"""
+    update.message.reply_text(text)
 
 
 start_handler = MessageHandler(Filters.command & Filters.regex('^/start'), start)
